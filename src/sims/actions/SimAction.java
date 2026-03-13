8ec13c5ca7fe8a593f16c61d82f38e542b89e875
Dave <dave21sg@gmail.com>
2026-03-13 15:51:58 +0800

    deleted classes

--- src/sims/actions/SimAction.java
+++
@@ -1,9 +0,0 @@
-package sims.actions;
-
-import sims.entity.SimProfile;
-
-// INTERFACE
-public interface SimAction {
-    public void execute(SimProfile currentSim);  // any action class must have this method
-    public String getDescription();  // description for display
-}
