e39d30d57b946d79140dc3303ea41ad7d05ae556
Dave <dave21sg@gmail.com>
2026-03-19 14:04:45 +0800

    added comments for readability.
    added createNeeds method to create default stats for sim creation

--- src/sims/gameEngine/SimFactory.java
+++ src/sims/gameEngine/SimFactory.java
@@ -3,9 +3,13 @@ package sims.gameEngine;
 import sims.actions.Activity;
 import sims.career.Career;
 import sims.entity.Sim;
+import sims.needs.need;
 import sims.world.Home;
 import sims.world.HomeLocation;
 import sims.world.HomeUpgrade;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimFactory {
     //simfactory to create home upgrades, home location, home and sim
@@ -15,14 +19,34 @@ public class SimFactory {
 
         Home home = new Home(name + "'s Home");
 
+        //Hunger
         Activity eatSandwich = new Activity("Sandwich", 30, "Hunger", 30);
+        //Upgrades
         Activity eatPizza = new Activity("Pizza", 10, "Hunger", 40);
-        Activity drinkCoffee = new Activity("Coffee", 10, "Energy", 20);
         Activity eatCake = new Activity("Pizza", 20, "Hunger", 50);
-        Activity watchTv = new Activity("Watch TV", 30, "Fun", 40);
-        Activity sleep = new Activity("Sleep",  100, "Energy", 80);
-        Activity nap = new Activity("Nap", 30, "Energy", 40);
 
+
+        //Fun
+        Activity watchTv = new Activity("Watch TV", 30, "Fun", 40);
+        //Upgrades
+        Activity cardGames = new Activity("Play Card Games", 15, "Fun", 20);
+
+        //Energy
+        Activity sleep = new Activity("Sleep",  100, "Energy", 80);
+        //Upgrades
+        Activity nap = new Activity("Nap", 30, "Energy", 40);
+        Activity drinkCoffee = new Activity("Coffee", 10, "Energy", 20);
+
+        //Social
+        Activity socialMedia = new Activity("Scroll Social Media", 15, "Social", 30);
+
+        //Hygiene
+        Activity shower = new Activity("Shower", 20, "Hygiene", 80);
+
+        //Bladder
+        Activity useToilet = new Activity("Use Toilet", 50, "Bladder", 100);
+
+        HomeLocation bathroom = new HomeLocation("Bathroom", home);
         HomeLocation livingRoom = new HomeLocation("Living Room", home);
         HomeLocation bedroom = new HomeLocation("Bedroom", home);
         HomeLocation kitchen = new HomeLocation("Kitchen", home);
@@ -30,14 +54,22 @@ public class SimFactory {
         HomeUpgrade oven = new HomeUpgrade("Kitchen", "Oven", 150, eatPizza);
         HomeUpgrade coffeeMachine = new HomeUpgrade("Kitchen", "Coffee Machine", 200, drinkCoffee);
         HomeUpgrade sofa = new HomeUpgrade("Bedroom", "Sofa", 300, nap);
+        HomeUpgrade cards = new HomeUpgrade("Living Room", "Cards", 50, cardGames);
 
         livingRoom.addActivity(watchTv);
+        livingRoom.addActivity(socialMedia);
+
         kitchen.addActivity(eatSandwich);
+
         bedroom.addActivity(sleep);
+
+        bathroom.addActivity(shower);
+        bathroom.addActivity(useToilet);
 
         bedroom.addUpgrade(sofa);
         kitchen.addUpgrade(coffeeMachine);
         kitchen.addUpgrade(oven);
+        livingRoom.addUpgrade(cards);
 
 
         //create activities
@@ -52,15 +84,29 @@ public class SimFactory {
         home.addHomeLocation(livingRoom);
         home.addHomeLocation(bedroom);
         home.addHomeLocation(kitchen);
+        home.addHomeLocation(bathroom);
         return home;
     }
 
     public static Sim createSim (String name, int simListSize, int age, Home home, Career career)
     {
         Sim sim = new Sim(name, simListSize, age);
+        sim.setNeeds(createNeeds());
         sim.setHome(home);
         sim.setCurrentLocation(home.getHomeLocation().getFirst());
         sim.setCareer(career);
         return sim;
+    }
+
+    public static Map<String, need> createNeeds ()
+    {
+        HashMap<String, need> needDict = new HashMap<>();
+        needDict.put("Energy", new need());
+        needDict.put("Social", new need());
+        needDict.put("Hygiene", new need());
+        needDict.put("Hunger", new need());
+        needDict.put("Fun", new need());
+        needDict.put("Bladder", new need());
+        return needDict;
     }
 }
